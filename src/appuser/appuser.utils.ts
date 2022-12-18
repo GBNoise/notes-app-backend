18be981ef41dcc6a7f50217adbebf8409725ed88
GBNoise <92615495+GBNoise@users.noreply.github.com>
2022-12-18 08:24:24 +0000

    changed some return types

--- src/appuser/appuser.utils.ts
+++ src/appuser/appuser.utils.ts
@@ -1,12 +1,13 @@
 import { Prisma, User } from '@prisma/client';
-import { AppResponse, API_PATH } from 'src/utils';
+import { AppResponse, API_PATH } from '../utils';
 
 export const APP_USER_CONTROLLER_ROUTE: string = API_PATH + '/users';
 
 export interface AppUserServiceInterface {
   createUser: (user: Prisma.UserCreateInput) => Promise<User | AppResponse>;
-  deleteUser: (id: string) => void;
-  updateUser: (user: Prisma.UserUpdateInput) => void;
+  deleteUser: (id: string) => Promise<User | AppResponse>;
+  deleteAllUsers: () => Promise<boolean>;
+  updateUser: (user: Prisma.UserUpdateInput) => Promise<User | AppResponse>;
   getUser: (id?: string, username?: string) => Promise<User | AppResponse>;
   getAllUsers: () => Promise<Array<User> | AppResponse>;
 }
