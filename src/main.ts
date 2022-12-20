1ee73e9e7bf3386b89ac57a45815d80a23e1084b
GBNoise <92615495+GBNoise@users.noreply.github.com>
2022-12-20 21:14:09 +0000

    configured views, static & template engine

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { NestExpressApplication } from '@nestjs/platform-express';
-
+import * as express from 'express';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.set('view engine', 'ejs');
+  app.use(express.static('public'));
+  app.setBaseViewsDir('views');
+  app.setViewEngine('hbs');
   await app.listen(8080);
 }
 bootstrap();
