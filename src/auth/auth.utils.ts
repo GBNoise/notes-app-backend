ce7e81868830366d4a4016187a1e21e06f6397d6
GBNoise <92615495+GBNoise@users.noreply.github.com>
2022-12-28 07:38:23 +0000

    created token duration constants and types

--- src/auth/auth.utils.ts
+++ src/auth/auth.utils.ts
@@ -3,8 +3,33 @@ export const jwtConstants = {
 };
 
 export const AUTH_CONTROLLER_ROUTE = "/auth"
+export const REFRESH_TOKEN_DURATION = "1s";
+export const ACCESS_TOKEN_DURATION = '2d';
 
 export enum Roles {
   ROLE_ADMIN = "ADMIN",
   ROLE_USER = "USER",
+}
+
+export declare type RefreshToken = {
+  id: string;
+  iat?: string | number;
+  exp?: string | number;
+}
+
+export declare type AccessToken = {
+  username: string;
+  email: string;
+  sub: string;
+  creationDate: string | Date;
+  updateDate: string | Date;
+  UserRoles: any[]
+  iat?: string | number;
+  exp?: string | number;
+}
+
+
+export declare type RawTokens = {
+  access_token?: string;
+  refresh_token?: string;
 }
