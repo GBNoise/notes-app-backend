d7030c7d81e205bf1aec0cefcaadb64d97294c0e
GBNoise <92615495+GBNoise@users.noreply.github.com>
2023-01-05 00:50:42 +0000

    documented utils

--- src/appuser/appuser.utils.ts
+++ src/appuser/appuser.utils.ts
@@ -3,6 +3,9 @@ import { AppResponse, API_PATH } from '../utils';
 
 export const APP_USER_CONTROLLER_ROUTE: string = API_PATH + '/users';
 
+/**
+ * Interface that contains the methods that the AppUserService should have
+ */
 export interface AppUserServiceInterface {
   createUser: (user: Prisma.UserCreateInput) => Promise<User | AppResponse>;
   deleteUser: (id: string) => Promise<User | AppResponse>;
@@ -12,7 +15,13 @@ export interface AppUserServiceInterface {
   getAllUsers: (options?: GetUserOptions) => Promise<Array<UserWithRoles | UserWithoutPassword> | AppResponse>;
 }
 
-
+/**
+ * Function that allows to filter the GetUserOptions
+ * @param options the options that the user wants to be included in the response through the url query
+ * @example // example of the url
+ * '${SERVER_URL}/api/${API_VERSION}/users?showRoles=true&take=10&page=1'
+ * @returns the filtered select object that can be used with prisma to get users from the database
+ */
 export const filterGetUserOptions = (options: GetUserOptions): { select: any, take: number, skip: number } => {
   let { showRoles, page, includeHashedPassword, take } = options;
   if (!showRoles && !includeHashedPassword) return;
@@ -48,11 +57,22 @@ export const filterGetUserOptions = (options: GetUserOptions): { select: any, ta
   return { select, take: Number(take), skip };
 }
 
-
+/**
+ * Utility function that validates if the user is ADMIN
+ * @param reqUser this should be the user from the request object a.k.a the logged user
+ * @returns 
+ */
 export const validateAdmin = (reqUser: any) => {
   return reqUser.UserRoles.some(({ role }) => role.name === "ADMIN");
 }
 
+/**
+ * Utility function that validates if the reqUser has either the same id or the same username passed to the function
+ * @param reqUser this should be the user from the request object a.k.a the logged user
+ * @param id the id passed to compare if it is the same as the reqUser.id in case of wanting to compare the id
+ * @param username the username passed to compare if it is the same as the reqUser.username in case of wanting to compare the username
+ * @returns true if it is the same user or false if it is not
+ */
 export const validateSameUser = (reqUser: any, id: string, username: string) => {
   const { id: loggedUserId, username: loggedUserUsername } = reqUser;
 
@@ -63,12 +83,16 @@ export const validateSameUser = (reqUser: any, id: string, username: string) => 
   return true
 }
 
+/**
+ * The valid options to filter the getUser methods
+ */
 export interface GetUserOptions {
   showRoles?: boolean | string;
   page?: number;
   take?: number;
   includeHashedPassword?: boolean | string
 }
+
 
 export interface UserWithRoles {
   id?: string,
