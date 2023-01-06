f11a5beab1ba0c28c7e20c7b8ed04ef05b391dbd
GBNoise <92615495+GBNoise@users.noreply.github.com>
2023-01-06 02:54:18 +0000

    notes service & controller

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, Req, Res, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, ForbiddenException, Get, Param, Post, Query, Req, Res, UseGuards } from "@nestjs/common";
 import { Prisma } from "@prisma/client";
 import { Response } from "express";
+import { validateAdmin, validateSameUser } from "src/appuser/appuser.utils";
 import { Roles } from "src/auth/auth.utils";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { HasRoles } from "src/auth/roles.decorator";
@@ -22,6 +23,61 @@ export class NotesController {
             return res.status(201).send(response);
         } catch (e) {
             return res.status(e.status).send(e)
+        }
+    }
+
+    @HasRoles(Roles.ROLE_ADMIN)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Get()
+    async getAllNotes(@Res() res: Response, @Query('take') take: number,
+        @Query("page") page: number) {
+        try {
+            const response = await this.notesService.getAllNotes({ take, page })
+            return res.status(200).send(response);
+        } catch (e) {
+            return res.status(e.status).send(e)
+        }
+    }
+
+    @HasRoles(Roles.ROLE_USER)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Get('/:userID')
+    async getUserNotes(@Res() res: Response, @Req() req: any, @Param("userID") userID: string) {
+        try {
+            const isSameUser = validateSameUser(req.user, userID, undefined);
+            // const isAdmin = validateAdmin(req.user);
+            if (!isSameUser) throw new ForbiddenException()
+
+            const response = await this.notesService.getUserNotes(userID);
+            return res.status(200).send(response);
+        } catch (e) {
+            return res.status(e.status).send(e);
+        }
+    }
+
+    @HasRoles(Roles.ROLE_USER)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Delete('/:userID')
+    async deleteUserNotes(@Res() res: Response, @Req() req: any, @Param("userID") userID: string) {
+        try {
+            const isSameUser = validateSameUser(req.user, userID, undefined);
+            if (!isSameUser) throw new ForbiddenException()
+            const response = await this.notesService.deleteUserNotes(userID);
+            return res.status(200).send(response);
+        } catch (e) {
+            return res.status(e.status).send(e)
+        }
+    }
+
+    @HasRoles(Roles.ROLE_ADMIN)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Delete()
+    async deleteAllUserNotes(@Res() res: Response) {
+        try {
+            const response = await this.notesService.deleteAllNotes();
+            return res.status(200).send(response);
+        } catch (e) {
+            return res.status(e.status).send(e);
         }
     }
 }
