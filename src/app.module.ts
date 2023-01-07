7a035f59adccb6249dba088efaa7b683607c00ad
GBNoise <92615495+GBNoise@users.noreply.github.com>
2023-01-07 10:06:15 +0000

    added events module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppUserModule } from './appuser/appuser.module';
 import { AuthModule } from './auth/auth.module';
 import { NotesModule } from './notes/notes.module';
 import { RolesModule } from './rolesmodel/roles.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [AppUserModule, AuthModule, RolesModule, NotesModule],
+  imports: [AppUserModule, AuthModule, RolesModule, NotesModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
